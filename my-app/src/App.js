7e63d8ac7f32c275b80e6f163bd1eea52e330f24
Vlad Kirichuk <vlad.kirichuk50@gmail.com>
2023-04-04 23:03:45 +0300

    files: index.js(add <div>JSX код</div>; App.js(add primitive const and functions for increment/decrement statement, add one counter and mutable string input).

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,32 @@
-function App() {
-  return (
-    <div className="App">
+import {useState} from "react";
 
-    </div>
+function App() {
+  const [likes, setLikes] = useState(5)
+  const [value, setValue] = useState('Текст в инпуте')
+
+  function increment() {
+    setLikes(likes + 1)
+
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
+  return (
+      <div className={"App"}>
+        <h1>{likes}</h1>
+        <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange = {event => setValue(event.target.value)}
+            /* {event => setValue(event.target.value)} - двухстороннее связывание состояний со значением в input.
+            * управляемый компонент (т.е. с изменяемым значением и состоянием.  */
+        />
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
   );
 }
 
